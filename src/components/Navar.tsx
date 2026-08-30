68b12e9ab5237e60a4b83377c41a785f0b8bace6
Milad-Parmoozeh <Milad.parmozeh@gmail.com>
2026-08-30 22:37:47 +0330

    navbar added

---
+++ src/components/Navar.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+function Navbar() {
+  const pathname = usePathname();
+
+  const navLinks = [
+    {
+      href: "/",
+      title: "Home",
+    },
+    {
+      href: "/store",
+      title: "Store",
+    },
+  ];
+
+  return (
+    <nav className="shadow p-4">
+      {navLinks.map((item) => (
+        <Link key={item.href} className={`mr-4 ${pathname=== item.href ? "text-sky-500": ""}`} href={item.href}>
+          {item.title}
+        </Link>
+      ))}
+    </nav>
+  );
+}
+
+export default Navbar;
