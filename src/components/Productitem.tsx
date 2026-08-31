c8d7046f17ec48af93381c788ed6976539fad2c8
Milad-Parmoozeh <Milad.parmozeh@gmail.com>
2026-08-31 23:16:34 +0330

    product page added

---
+++ src/components/Productitem.tsx
@@ -0,0 +1,24 @@
+interface IProductItemProps {
+  id: string;
+  image: string;
+  title: string;
+  description: string;
+  price: number;
+}
+
+const Productitem = ({image,price,title}: IProductItemProps) => {
+  return (
+    <div className="shadow-md">
+      <img src={image} />
+
+      <div className="p-2 text-right rtl">
+        <h3 className="font-bold"> {title} </h3>
+        <p>
+          قیمت: <span> ${price} </span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Productitem;
