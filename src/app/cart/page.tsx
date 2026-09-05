ea2b32ed7689b5ce3b6703aa727286525ccefbe8
Milad-Parmoozeh <Milad.parmozeh@gmail.com>
2026-09-05 22:55:10 +0330

    discount price added

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -7,11 +7,15 @@ import { useShoppingCartContext } from "@/context/ShoppingCartContext";
 import axios from "axios";
 import { log } from "node:console";
 import { useEffect, useState } from "react";
+interface IDiscountData{ id:number;code:string;percentage:number}
 
 function Cart() {
   const {cartItems}= useShoppingCartContext();
   
   const [data, setData] = useState<IProductItemProps[]>([]);
+  const [discountCode, setDiscountCode] = useState("")
+  const [finalPrice, setFinalPrice] = useState(0)
+  const [discountedPeice, setDiscountedPeice] = useState(0)
 
   useEffect(()=>{
     axios(`http://localhost:3004/products`).then(result=>{
@@ -22,8 +26,28 @@ function Cart() {
     })
   },[])
 
-  
+  let totalPrice=cartItems.reduce((total,item)=>{
 
+            let selectedProduct=data.find((product)=> product.id == item.id.toString())
+
+            return total+ (selectedProduct?.price || 0)* item.qty
+
+          }, 0)
+
+  
+  const handleSubmitDiscount= ()=>{
+    axios(`http://localhost:3004/discounts?code=${discountCode}`).then(result=>{
+      console.log(result);
+      const data= result.data as IDiscountData[]
+      let discountedPeice=totalPrice *data[0].percentage/100;
+      let finalPrice= totalPrice-discountedPeice
+      setFinalPrice(finalPrice)
+      setDiscountedPeice(discountedPeice)
+      
+    })
+    
+    
+  }
 
 
 
@@ -46,28 +70,22 @@ function Cart() {
           {" "}
           قیمت کل: <span>{
           
-          formatNumberWithCommas(cartItems.reduce((total,item)=>{
-
-            let selectedProduct=data.find((product)=> product.id == item.id.toString())
-
-            return total+ (selectedProduct?.price || 0)* item.qty
-
-          }, 0))}$
+          formatNumberWithCommas(totalPrice)}$
           </span>{" "}
           
         </h3>
         
         <h3 className="rtl">
           {" "}
-          سود شما از خرید: <span>365$</span>{" "}
+          سود شما از خرید: <span>{formatNumberWithCommas(discountedPeice)}$</span>{" "}
         </h3>
         <h3 className="rtl">
           {" "}
-          قیمت نهایی: <span>365$</span>{" "}
+          قیمت نهایی: <span>{formatNumberWithCommas(finalPrice)}$</span>{" "}
         </h3>
 
         <div>
-          <button className="bg-sky-600 text-white px-4 py-1 rounded">
+          <button onClick={handleSubmitDiscount} className="bg-sky-600 text-white px-4 py-1 rounded">
             {" "}
             اعمال کد تخفیف{" "}
           </button>
@@ -75,6 +93,7 @@ function Cart() {
             className=" text-right border"
             placeholder="کد تخفیف را وارد کنید"
             type="text"
+            onChange={(e)=>setDiscountCode(e.target.value)}
           />
         </div>
       </div>
