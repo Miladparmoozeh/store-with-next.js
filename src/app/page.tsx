54707e09b348f7437d8da24d4bfa71bc924754aa
Milad-Parmoozeh <Milad.parmozeh@gmail.com>
2026-09-09 17:03:56 +0330

    finall

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,12 @@
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="flex items-center justify-center h-100 mb-1.5">
+      <div className="w-1/2 h-1/2-translate-y-64 bg-sky-100 rounded-2xl shadow-xl p-8 flex items-center justify-center">
+          <h1 className="text-4xl font-bold text-sky-800">
+            Wellcome babe
+          </h1>
+      </div>
     </div>
   );
 }
